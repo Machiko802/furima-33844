a640ed0d0529e16e283a318f5c0dcbe1849d1397
Machiko802 <machiko0831@gmail.com>
2021-02-28 12:13:53 +0900

    ユーザー登録の際に必要な正規表現を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々]+\z/, message: '全角文字を使ってください' } do
+           validates :first_name, presence: true
+           validates :last_name, presence: true
+         end
+
+         with_options presence: true, format: { with: /\A[ァ-ヶー-]+\z/, message: '全角カタカナを使ってください' } do
          validates :first_name_kana, presence: true
          validates :last_name_kana, presence: true
+         end
+
+         with_options presence: true, format: { with: /[a-z\d]{6,}/i, message: '半角英数字混合で入力してください' } do
+         validates :encrypted_password
+         end
+
+         validates :nickname, presence: true 
          validates :birthday, presence: true
+
         
 end
