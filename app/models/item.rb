a963ebf2034810e09da163a92e001d5782768e4a
Machiko802 <machiko0831@gmail.com>
2021-03-04 16:45:23 +0900

    priceのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,27 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :text
-    validates :category_id
-    validates :status_id
-    validates :delivery_fee_id
-    validates :prefecture_id
-    validates :delivery_days_id 
-    validates :price, numericality: {only_integer:true, greater_htan: 300, less_than: 10000000}
+    validates :image
+    validates :price, format: { with: /\A[0-9]+\z/ },
+                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  belongs_to :user
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :delivery_fee_id
+    validates :delivery_day_id
+    validates :prefecture_id
+    validates :status_id
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery_fee
+  belongs_to :delivery_day
+  belongs_to :prefecture
+  belongs_to :status
 end
