d1d282071fddb88f4894437e1d540a06f21b769c
Machiko802 <machiko0831@gmail.com>
2021-03-05 11:17:43 +0900

    重複していたバリデーションを削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,17 +6,18 @@ class Item < ApplicationRecord
     validates :name
     validates :text
     validates :image
-    validates :price, format: { with: /\A[0-9]+\z/ },
-                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  with_options presence: true, numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :delivery_fee_id
     validates :delivery_day_id
     validates :prefecture_id
     validates :status_id
   end
+
+  validates :price, format: { with: /\A[0-9]+\z/ },
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
