b04f1db03e73d65166c3ec1f2585cffc206e6912
Machiko802 <machiko0831@gmail.com>
2021-03-06 16:07:26 +0900

    destroyアクションを記述＆ログインしないと削除できないように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
 
   def index
     @items = Item.all.order(id: 'DESC')
@@ -16,6 +16,15 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    Item.destroy(params[:id])
+    redirect_to root_path
   end
 
   private
