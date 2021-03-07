433e65f3780978dcd2bad3001919562e467e43f9
Machiko802 <machiko0831@gmail.com>
2021-03-07 17:42:36 +0900

    updateアクションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    end
+  end
+
   def destroy
     item = Item.find(params[:id])
     if current_user.id == item.user_id
