8bb944b9a54ea4e9af499881d63970ea8aca0f94
Machiko802 <machiko0831@gmail.com>
2021-03-13 14:50:11 +0900

    purchase_recordのマイグレーションファイルにカラムを追加

--- db/migrate/20210313053126_create_purchase_records.rb
+++ db/migrate/20210313053126_create_purchase_records.rb
@@ -1,7 +1,8 @@
 class CreatePurchaseRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :purchase_records do |t|
-      
+      t.references :user, nul: false, foreign_key: true
+      t.references :item, nul: false, foregin_key: true
       t.timestamps
     end
   end
