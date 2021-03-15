4d9ec9064e926ca06e4041c86e20dfe9354f2b11
Machiko802 <machiko0831@gmail.com>
2021-03-15 12:43:44 +0900

    Payjpに購入情報を送信する記述を、purchase_records_controller.rbに追記

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,14 +1,14 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_record_address = PurchaseRecordAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_record_address = PurchaseRecordAddress.new(purchase_params)
     if @purchase_record_address.valid?
+      pay_item
        @purchase_record_address.save
        redirect_to root_path
     else
@@ -18,7 +18,20 @@ class PurchaseRecordsController < ApplicationController
 
   private
 
+  def item
+    @item = Item.find(params[:item_id])
+  end
+
   def purchase_params
-    params.require(:purchase_record_address).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id )
+    params.require(:purchase_record_address).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token] )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item[:price],
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
   end
 end
