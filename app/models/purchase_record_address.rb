b295b95c89d2b3a2f7ade5552fb2efe4fcd29175
Machiko802 <machiko0831@gmail.com>
2021-03-15 12:46:41 +0900

    PurchaseRecordAddress　ActiveModelを編集
    ・tokenの値が遅れるように追記
    ・tokenのバリデーションを追記

--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -1,6 +1,6 @@
 class PurchaseRecordAddress
   include ActiveModel::Model 
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number, :token
 
   with_options presence: true do
     validates :user_id
@@ -8,6 +8,7 @@ class PurchaseRecordAddress
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :block
+    validates :token
   end
   validates :phone_number, numericality: {with: /\A[0-9]{11}\z/, message: "can't be blank"}
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
