cf09eb6e0646ed70ad4b0d4800c7c6d85883c5fd
Machiko802 <machiko0831@gmail.com>
2021-03-17 15:17:11 +0900

    電話番号のバリデーションを修正
    ・文字数の制限を修正
    ・適切なエラー文が出るように実装

--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -9,8 +9,9 @@ class PurchaseRecordAddress
     validates :city
     validates :block
     validates :token
+    validates :phone_number, numericality: { only_integer: true }, length: { is: 11 }
   end
-  validates :phone_number, numericality: {with: /\A[0-9]{11}\z/, message: "can't be blank"}
+  
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save           
