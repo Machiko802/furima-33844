b8ea77894fb8783559c450765c692a4fc48068ef
Machiko802 <machiko0831@gmail.com>
2021-03-17 22:45:54 +0900

    FacroryBotの外部キーの記述を削除

--- spec/factories/purchase_record_addresses.rb
+++ spec/factories/purchase_record_addresses.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :purchase_record_address do
-    user_id        { Faker::Number.number(digits: 2) }
-    item_id        { Faker::Number.number(digits: 2) }
     post_code      { '123-0012' }
     prefecture_id  { Faker::Number.within(range: 2..48) }
     city           { '豊島区' }
